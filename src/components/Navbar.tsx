39453674c2502f8f6d45473a6ea78601da98ec03
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-01-01 21:51:31 +0300

    add Hero component; enhance Navbar styling; include corner image; add react-router-dom dependency

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,37 @@
+import { IconMenuDeep } from "@tabler/icons-react";
+
 function Navbar() {
   return (
-    <div>
-      <h1 className="font-titles">Navbar</h1>
+    <div className="bg-gradient-to-b from-[#404040] to-[#1A1A1A] rounded-[20px] w-full lg:w-[70%] md:w-[90%] flex items-center justify-between py-4 mt-4 shadow-sm shadow-[#1a1a1a]">
+      <div className="flex items-center">
+        <img src="/logo.svg" alt="Logo" className="h-[30px] w-[30px] ml-4" />
+        <h1 className="font-titles font-bold text-white text-2xl ml-2">
+          <span className="text-[#7FC786]">The</span>
+          Chama
+          <span className="text-[#7FC786]">DAO</span>
+        </h1>
+      </div>
+      <div className="items-center gap-2 hidden md:flex">
+        <div>
+          <button className=" text-white font-titles font-bold text-sm px-4 py-2 rounded-md mr-4">
+            About
+          </button>
+          <button className=" text-white font-titles font-bold text-sm px-4 py-2 rounded-md mr-4">
+            Contact
+          </button>
+          <button className=" text-white font-titles font-bold text-sm px-4 py-2 rounded-md mr-4">
+            Invest
+          </button>
+        </div>
+        <button className="bg-[#FCE9B6] text-[#000] font-titles font-bold text-sm px-4 py-2 rounded-md mr-4">
+          Sign Up
+        </button>
+      </div>
+      <div className="flex md:hidden">
+        <button className="text-white font-titles font-bold text-sm px-4 py-2 rounded-md mr-4">
+          <IconMenuDeep size={24} />
+        </button>
+      </div>
     </div>
   );
 }
--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,9 +1,16 @@
+import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 
 function Landing() {
   return (
-    <div>
-      <Navbar />
+    <div className="overflow-hidden">
+      <div className="absolute top-0 left-0 right-0 z-10 flex justify-center items-center">
+        <Navbar />
+      </div>
+      <section>
+        <Hero />
+      </section>
+      <img src="/corner.png" alt="Corner" className="absolute top-0 right-0" />
     </div>
   );
 }
