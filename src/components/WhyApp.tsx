c103f12180990024a20c3af866bedcaa8ce26073
divin3circle <omutelema.silas21@students.dkut.ac.ke>
2025-01-02 18:03:22 +0300

    refactor WhyApp component layout; adjust image and background dimensions for improved responsiveness

--- src/components/WhyApp.tsx
+++ src/components/WhyApp.tsx
@@ -21,15 +21,15 @@ function WhyApp() {
     },
   ];
   return (
-    <div className="lg:max-w-[1040px] mx-auto my-0">
+    <div className="lg:max-w-[1040px] w-full mx-auto my-0">
       <div className="flex flex-col md:flex-row md:justify-between justify-center mt-16">
         <div className="flex justify-center items-center relative flex-1 mb-8 md:mb-0">
           <img
             src="/hero1.svg"
             alt="Hero"
-            className="h-[400px] md:w-[400px] w-full relative z-[1]"
+            className="h-[400px] md:w-[350px] w-full relative z-[1]"
           />
-          <div className="bg-[#FAE8B6] md:w-[400px] w-full h-[420px] absolute rounded-[24px] transform rotate-[6.9deg] top-0 bottom-0"></div>
+          <div className="bg-[#FAE8B6] md:w-[350px] w-full h-[400px] absolute rounded-[24px] transform rotate-[6.9deg] top-10 left-0 bottom-0"></div>
         </div>
         <div className="flex flex-col flex-1">
           <h1 className="text-white font-body bg-[#363636] p-1 text-4xl lg:text-5xl w-full lg:w-[500px] md:w-[400px] mt-4 md:my-2 md:mt-0 font-bold">
